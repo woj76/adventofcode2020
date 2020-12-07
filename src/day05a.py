e2735a17d0bd4f8c745dac88de8bae755f58b7e0
Wojciech Mostowski <wojciech.mostowski@gmail.com>
2020-12-07 18:58:29 +0100

    Day 5a can be even simpler, tiny bit

--- src/day05a.py
+++ src/day05a.py
@@ -3,7 +3,7 @@
 from aoc import repres
 
 file = open("../data/day05.txt", "rt")
-data = [int("0b" + x, 2) for x in file.read().strip().replace('B','1').replace('F','0').replace('R','1').replace('L','0').split('\n')]
+data = [int(x, 2) for x in file.read().strip().replace('B','1').replace('F','0').replace('R','1').replace('L','0').split('\n')]
 file.close()
 
 part2 = True
